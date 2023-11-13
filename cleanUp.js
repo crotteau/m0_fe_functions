0817b4109eaacd6659dac059300b2230d178c74f
Megan Crotteau <megan.crotteau@gmail.com>
2023-11-12 21:48:48 -0700

    Made improvements to existing code

--- cleanUp.js
+++ cleanUp.js
@@ -8,23 +8,29 @@
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+// Unsure if this is too simple of a change, but I formatted the code so it's easier to read and I added semi-colons.
 
-askForName()
+function askForName() { 
+  console.log("Hello, what is your name?"); }
+
+askForName();
 
 
 // EX 2:
+// I added console.log so that both return values for the addThreeNums functions were listed. 
+// Before, only the final return value was listed in the console. 
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+var sum = first + second + third;
 return sum;
       }
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+console.log(addThreeNums(1, 2, 3));
+console.log(addThreeNums(4, 2, 7));
 
 
 // EX 3:
-func makeFreshPesto(){
+// I completed the word "function" (originally it only said "func"). This allowed the function to appear properly.
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
@@ -35,6 +41,7 @@ makeFreshPesto();
 
 
 //  EX 4:
+// I defined values for the variables num1 and num2 and then requested that return value was visible in the console.
 function average(num1, num2) 
   {
 var sum = num1 + num2;
@@ -42,3 +49,7 @@ var sum = num1 + num2;
 
 return avg
   }
+
+  console.log(average(234, 333));
+
+  
